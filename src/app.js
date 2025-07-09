35084f8148bc31e88eb1b9fe8d89a5dab16c2cf2
Aya Sulyman <ayasulyiman@gmail.com>
2025-07-09 15:59:45 +0300

    .

--- src/app.js
+++ src/app.js
@@ -20,14 +20,14 @@ app.use(bodyParser.json())
 
 
 
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }).then(() => {
-//   console.log("Connected to MongoDB");
-// }).catch((error) => {
-//   console.error("MongoDB connection error:", error);
-// });
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => {
+  console.error("MongoDB connection error:", error);
+});
 
   
 const verifyRefreshToken = (req, res, next) => {
