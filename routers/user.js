c217a7ac2faea7ccb27bac85555d2a62b86ecbe8
Aya Sulyman <ayasulyiman@gmail.com>
2025-08-06 12:14:44 +0300

    .

--- routers/user.js
+++ routers/user.js
@@ -1130,7 +1130,6 @@ router.post('/change-language', authenticateToken, async (req, res) => {
 });
 
 
-const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../docs/swagger');
 
